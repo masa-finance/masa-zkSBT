39432bc38480bb7328a12cb2efb1da8b5033c6cf
Miquel A. Cabot <miquel.cabot@gmail.com>
2023-04-13 11:56:07 +0200

    add loanEligible() function

--- contracts/VerifyCreditScore.sol
+++ contracts/VerifyCreditScore.sol
@@ -19,4 +19,31 @@ contract VerifyCreditScore {
     constructor(IVerifier _verifier) {
         verifier = _verifier;
     }
+
+    // @notice verifies the validity of the proof, and make further verifications on the public
+    // input of the circuit, if verified add the address to the list of eligible addresses
+    function loanEligible(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[73] memory publicValues,
+        uint256 threshold
+    ) public view {
+        require(
+            publicValues[0] ==
+                0x0000000000000000000000000000000000000000000000000000000000000001,
+            "The claim doesn't satisfy the query condition"
+        );
+        require(
+            publicValues[2] == threshold,
+            "Invalid threshold value used to generate the proof"
+        );
+
+        require(
+            verifier.verifyProof(a, b, c, publicValues),
+            "Proof verification failed"
+        );
+
+        // isElegibleForAirdrop[msg.sender] = true;
+    }
 }

b8bbf6935d74f99b859ef1b5218f43a8b734e6c9
Miquel A. Cabot <miquel.cabot@gmail.com>
2023-04-13 11:42:09 +0200

    add verifier deployment script

---
+++ deploy/Verifier.ts
@@ -0,0 +1,51 @@
+import hre from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { DeployFunction } from "hardhat-deploy/dist/types";
+import { getEnvParams } from "../src/EnvParams";
+
+let admin: SignerWithAddress;
+
+const func: DeployFunction = async ({
+  // @ts-ignore
+  getNamedAccounts,
+  // @ts-ignore
+  deployments,
+  // @ts-ignore
+  ethers,
+  network
+}) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  [, admin] = await ethers.getSigners();
+  const env = getEnvParams(network.name);
+
+  const constructorArguments = [];
+
+  const verifierDeploymentResult = await deploy("Verifier", {
+    from: deployer,
+    args: constructorArguments,
+    log: true
+  });
+
+  // verify contract with etherscan, if its not a local network
+  if (network.name !== "hardhat") {
+    try {
+      await hre.run("verify:verify", {
+        address: verifierDeploymentResult.address,
+        constructorArguments
+      });
+    } catch (error) {
+      if (
+        !error.message.includes("Contract source code already verified") &&
+        !error.message.includes("Reason: Already Verified")
+      ) {
+        throw error;
+      }
+    }
+  }
+};
+
+func.tags = ["Verifier"];
+func.dependencies = [];
+export default func;

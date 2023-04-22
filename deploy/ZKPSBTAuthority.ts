e54504859cd64d3c57315d3bad5971c3b15670d8
Miquel A. Cabot <miquel.cabot@gmail.com>
2023-04-22 20:04:51 +0200

    add ZKPSBTAuthority deployment script

---
+++ deploy/ZKPSBTAuthority.ts
@@ -0,0 +1,81 @@
+import hre from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { DeployFunction } from "hardhat-deploy/dist/types";
+import { getEnvParams, getPrivateKey } from "../src/EnvParams";
+
+let admin: SignerWithAddress;
+
+const func: DeployFunction = async ({
+  // @ts-ignore
+  getNamedAccounts,
+  // @ts-ignore
+  deployments,
+  // @ts-ignore
+  ethers,
+  network
+}) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  [, admin] = await ethers.getSigners();
+  const env = getEnvParams(network.name);
+  const baseUri = `${env.BASE_URI}`;
+
+  const constructorArguments = [
+    env.ADMIN || admin.address,
+    env.SBT_NAME,
+    env.SBT_SYMBOL,
+    baseUri,
+    ethers.constants.AddressZero,
+    [
+      env.SWAP_ROUTER,
+      env.WETH_TOKEN,
+      env.USDC_TOKEN,
+      env.MASA_TOKEN,
+      env.RESERVE_WALLET || admin.address
+    ]
+  ];
+
+  const zkpSBTAuthorityDeploymentResult = await deploy("ZKPSBTAuthority", {
+    from: deployer,
+    args: constructorArguments,
+    log: true
+  });
+
+  // verify contract with etherscan, if its not a local network
+  if (network.name !== "hardhat") {
+    try {
+      await hre.run("verify:verify", {
+        address: zkpSBTAuthorityDeploymentResult.address,
+        constructorArguments
+      });
+    } catch (error) {
+      if (
+        !error.message.includes("Contract source code already verified") &&
+        !error.message.includes("Reason: Already Verified")
+      ) {
+        throw error;
+      }
+    }
+  }
+
+  if (network.name === "hardhat" || network.name === "goerli") {
+    const signer = env.ADMIN
+      ? new ethers.Wallet(getPrivateKey(network.name), ethers.provider)
+      : admin;
+
+    const zkpSBTAuthority = await ethers.getContractAt(
+      "ZKPSBTAuthority",
+      zkpSBTAuthorityDeploymentResult.address
+    );
+
+    // add authority to ZKPSBTAuthority
+    await zkpSBTAuthority
+      .connect(signer)
+      .addAuthority(env.AUTHORITY_WALLET || admin.address);
+  }
+};
+
+func.tags = ["ZKPSBTAuthority"];
+func.dependencies = [];
+export default func;

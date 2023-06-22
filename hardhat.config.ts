82cc9b6e14484502c10f73bb64662b02ac8001f9
Miquel A. Cabot <miquel.cabot@gmail.com>
2023-06-22 09:58:38 +0200

    add compiler version 0.6.11

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -42,6 +42,18 @@ export default {
   solidity: {
     compilers: [
       {
+        version: "0.6.11",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1,
+            details: {
+              yul: false
+            }
+          }
+        }
+      },
+      {
         version: "0.8.8",
         settings: {
           optimizer: {

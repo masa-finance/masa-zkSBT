bb51b4a14c4df208977a6c650b771c37e68bafc9
Miquel A. Cabot <miquel.cabot@gmail.com>
2023-06-28 11:19:13 +0200

    add ZKPSBTAuthorityIPFS deployment script

---
+++ deploy/ZKPSBTAuthorityIPFS.ts
@@ -0,0 +1,71 @@
+import hre from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { DeployFunction } from "hardhat-deploy/dist/types";
+import { getEnvParams, getPrivateKey } from "../src/EnvParams";
+
+let admin: SignerWithAddress;
+
+const func: DeployFunction = async ({
+  // @ts-ignore
+  getNamedAccounts,
+  // @ts-ignore
+  deployments,
+  // @ts-ignore
+  ethers,
+  network
+}) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  [, admin] = await ethers.getSigners();
+  const env = getEnvParams(network.name);
+  const baseUri = `${env.BASE_URI}`;
+
+  const constructorArguments = [
+    env.ADMIN || admin.address,
+    env.SBT_NAME,
+    env.SBT_SYMBOL,
+    baseUri,
+    ethers.constants.AddressZero,
+    [
+      env.SWAP_ROUTER,
+      env.WETH_TOKEN,
+      env.USDC_TOKEN,
+      env.MASA_TOKEN,
+      env.PROJECTFEE_RECEIVER || admin.address,
+      env.PROTOCOLFEE_RECEIVER || ethers.constants.AddressZero,
+      env.PROTOCOLFEE_AMOUNT || 0,
+      env.PROTOCOLFEE_PERCENT || 0
+    ]
+  ];
+
+  const zkpSBTAuthorityIPFSDeploymentResult = await deploy(
+    "ZKPSBTAuthorityIPFS",
+    {
+      from: deployer,
+      args: constructorArguments,
+      log: true
+    }
+  );
+
+  // verify contract with etherscan, if its not a local network
+  if (network.name !== "hardhat") {
+    try {
+      await hre.run("verify:verify", {
+        address: zkpSBTAuthorityIPFSDeploymentResult.address,
+        constructorArguments
+      });
+    } catch (error) {
+      if (
+        !error.message.includes("Contract source code already verified") &&
+        !error.message.includes("Reason: Already Verified")
+      ) {
+        throw error;
+      }
+    }
+  }
+};
+
+func.tags = ["ZKPSBTAuthorityIPFS"];
+func.dependencies = [];
+export default func;
